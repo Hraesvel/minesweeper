ffbd7b10cd54003e21a4bb82d4c81fb7f40b9a42
Martin <msmith3dart@icloud.com>
2019-09-16 15:38:14 -0700

    updated demo.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,25 +1,26 @@
 use minesweeper::*;
 use std::io;
-use std::io::{BufReader, Read, Write};
+use std::io::{BufReader, Read, Write, Split};
 
 fn main() -> io::Result<()> {
 	let mut buf = String::with_capacity(8);
 	let mut stdout = io::stdout();
-	let mut session = Session::new(12, 12, 12);
+	let mut session = Session::new(60, 24, 24);
 	loop {
 		session.print_session();
 		stdout.lock().write_all(b"$> ")?;
 		stdout.flush()?;
-		let len = io::stdin().read_line(&mut buf)?;
+		let len = io::stdin().read_line(&mut buf)? - 1;
 
-		let mut token = buf.trim().split(|c| c == ',' || c == ' ');
-		let x = token.next().unwrap().parse().expect("Num");
-		let y = token.next().unwrap().parse().expect("Num");
-		if len > 2 {
-			session.check_cord(x, y);
-			//			stdout
-			//				.lock()
-			//				.write(buf.as_bytes())?;
+		if len >= 2 {
+			let mut token = buf.trim().split(|c| c == ',' || c == ' ');
+			let x = token.next().unwrap().parse().expect("Num");
+			let y = token.next().unwrap().parse().expect("Num");
+			if !session.check_cord(x, y) {
+				session.print_answer_fancy();
+				println!("You Dead!");
+				break;
+			}
 		}
 		buf.clear();
 	}
